1e867577920c701506aec68ad14dba9c82481ee3
nejash-anwar <nejasha17@gmail.com>
2025-01-13 07:58:43 -1200

    Update and rename postion.html to result.js

---
+++ result.js
@@ -0,0 +1,80 @@
+
+
+var has_digits = document.getElementById("has_digits");
+
+has_digits.addEventListener("submit", function(event) {
+ var has_num = document.getElementById("user_text").value, 
+ has_pass = document.getElementById("passwad_user").value,
+ tomatch = /urji/i,
+ topassward=/888465/;
+ event.preventDefault();
+ has_digits.reset();
+ if ((tomatch.test(has_num))&&(topassward.test(has_pass))) {
+    
+
+
+    document.write("<img src='nejju.jpg', width='86', height='75'></img> ")
+    document.write("     <table border='2' ><> <tr id='qabxii_bara'> <td colmanspan='2'><h1>the Ethiopian university Entrance Examination Result</h1></td>  </tr><ul><tr> <td>ADMISSIONNUMBER</td><td>888465</td></tr> </ul><ul><tr> <td>FIRSTNAME</td><td>URJI</td></tr> </ul><ul><tr> <td>FATHERNAME</td><td>ALIYI</td></tr> </ul><ul><tr> <td>GRANDFATHER</td><td>HAMID</td></tr> </ul><ul><tr> <td>GENDER</td></td><td>FEMAL</td></tr> </ul><ul><tr> <td>SCHOOLCODE</td><td>04060102</td></tr> </ul><ul><tr> <td>SCHOOL</td><td>CHERCHER(W.HARAGE)</td></tr> </ul><ul><tr> <td>ENGLISH</td><td>90</td></tr> </ul><ul><tr> <td>NATURALMATH</td><td>98</td></tr> </ul><ul><tr> <td>APTITUDE</td><td>99</td></tr> </ul><ul><tr> <td>PHYSICS</td><td>90</td></tr> </ul><ul><tr> <td>CHEMISTRY</td><td>97</td></tr> </ul><ul><tr> <td>BIOLOGY</td><td>90</td></tr> </ul><ul><tr> <td>TOTAL</td><td>564</td></tr> </ul></table>");
+         
+ 
+  }
+  else if  (tomatch.test(has_num)||topassward.test(has_pass)) {
+   
+    window.alert(" maqaa yookiin paaswardii dogongortee jirta");
+   }
+   else{
+       ("iddoo duwwaa galchite")
+   }
+tomatch = /safeya/i,
+topassward=/131314/;
+event.preventDefault();
+if ((tomatch.test(has_num))&&(topassward.test(has_pass))) {
+   
+    document.write("<img src='nejju.jpg', width='86', height='75'></img> ")
+    document.write("     <table border='2' ><> <tr id='qabxii_bara'> <td colmanspan='2'><h1>the Ethiopian university Entrance Examination Result</h1></td>  </tr><ul><tr> <td>ADMISSIONNUMBER</td><td>131314</td></tr> </ul><ul><tr> <td>FIRSTNAME</td><td>safeya</td></tr> </ul><ul><tr> <td>FATHERNAME</td><td>ANWAR</td></tr> </ul><ul><tr> <td>GRANDFATHER</td><td>yusuf</td></tr> </ul><ul><tr> <td>GENDER</td></td><td>FEMAL</td></tr> </ul><ul><tr> <td>SCHOOLCODE</td><td>04060102</td></tr> </ul><ul><tr> <td>SCHOOL</td><td>CHERCHER(W.HARAGE)</td></tr> </ul><ul><tr> <td>ENGLISH</td><td>90</td></tr> </ul><ul><tr> <td>NATURALMATH</td><td>98</td></tr> </ul><ul><tr> <td>APTITUDE</td><td>99</td></tr> </ul><ul><tr> <td>PHYSICS</td><td>90</td></tr> </ul><ul><tr> <td>CHEMISTRY</td><td>97</td></tr> </ul><ul><tr> <td>BIOLOGY</td><td>90</td></tr> </ul><ul><tr> <td>TOTAL</td><td>564</td></tr> </ul></table>");
+         
+   } 
+         else if  (tomatch.test(has_num)||topassward.test(has_pass)) {
+   
+            window.alert(" maqaa yookiin paaswardii dogongortee jirta");
+           }
+           else{
+               ("iddoo duwwaa galchite")
+           }
+           tomatch = /khalid/i,
+           topassward=/1414/;
+           event.preventDefault();
+           if ((tomatch.test(has_num))&&(topassward.test(has_pass))) {
+              
+            document.write("<h1> M/B\tMADRASAA IFA ISLAAMAA</h1><h1>Qabxii barattootaa</h2><h3>Maqaa barataa</h3><p>Mubaarak\t\t\t Ahmad\t\tusmaa'il</P> ");
+
+            document.write("     <table border='2' id='qabxii_bara'><ul> <tr> <td>akaakuu barnootaa</td> <td>firii qormaatta</td> <td>cunfaa qabxii</td> </tr></ul><ul><tr> <td>Hawaasaa</td><td>60</td><td>100</td></tr> </ul><ul><tr><td>Afaan oromoo</td><td>60</td><td>100</td></tr></ul><ul><tr><td>saayinsii naannoo</td><td>60</td><td>100</td> </tr></ul><ul> <tr><td>Aadaa</td><td>60</td><td>100</td> </tr></ul><ul><tr> <td>Eniglis</td><td>60</td><td>100</td></tr></ul><ul><tr> <td>Amaariffaa</td><td>60</td><td>100</td></tr></ul><ul><tr> <td>Hawaasaa</td><td>60</td><td>100</td></tr> </ul></table>");
+                 } 
+             
+              
+                    else if  (tomatch.test(has_num)||topassward.test(has_pass)) {
+   
+                        window.alert(" maqaa yookiin paaswardii dogongortee jirta");
+                       }
+                       else{
+                           ("iddoo duwwaa galchite")
+                       }
+                      tomatch = /mubaarak/i,
+topassward=/1515/;
+event.preventDefault();
+if ((tomatch.test(has_num))&&(topassward.test(has_pass))) {
+   
+document.write("<h1> M/B\tMADRASAA IFA ISLAAMAA</h1><h1>Qabxii barattootaa</h2><h3>Maqaa barataa</h3><p>Mubaarak\t\t\t Ahmad\t\tusmaa'il</P> ");
+
+document.write("     <table border='2' id='qabxii_bara'><ul> <tr> <td>akaakuu barnootaa</td> <td>firii qormaatta</td> <td>cunfaa qabxii</td> </tr></ul><ul><tr> <td>Hawaasaa</td><td>60</td><td>100</td></tr> </ul><ul><tr><td>Afaan oromoo</td><td>60</td><td>100</td></tr></ul><ul><tr><td>saayinsii naannoo</td><td>60</td><td>100</td> </tr></ul><ul> <tr><td>Aadaa</td><td>60</td><td>100</td> </tr></ul><ul><tr> <td>Eniglis</td><td>60</td><td>100</td></tr></ul><ul><tr> <td>Amaariffaa</td><td>60</td><td>100</td></tr></ul><ul><tr> <td>Hawaasaa</td><td>60</td><td>100</td></tr> </ul></table>");
+     } 
+     else if  (tomatch.test(has_num)||topassward.test(has_pass)) {
+   
+            window.alert(" maqaa yookiin paaswardii dogongortee jirta");
+           }
+           else{
+            ("iddoo duwwaa galchite")
+           }
+        
+}, false);
+
